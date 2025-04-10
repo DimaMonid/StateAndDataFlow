cbf32cd809ff52bf8f890fa16f475fa0cea66e95
Dima Monid <1234567waka.waka@gmail.com>
2025-04-10 17:53:28 +0300

    Initial Commit

---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by Дима Монид on 10.04.25.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
